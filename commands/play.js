fb0dd615fbe827c88e623966a29dda3096aa7d89
TokenMinal <ludocrashnadeau@gmail.com>
2022-03-16 18:44:28 -0400

    el switchirino

--- commands/play.js
+++ commands/play.js
@@ -16,7 +16,7 @@ module.exports = {
     try {
       if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
         return void interaction.reply({
-          content: 'You are not in a voice channel!',
+          content: 'https://media.discordapp.net/attachments/938910538799382629/945000454377582654/caption.gif',
           ephemeral: true,
         });
       }
@@ -26,7 +26,7 @@ module.exports = {
         interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
       ) {
         return void interaction.reply({
-          content: 'You are not in my voice channel!',
+          content: 'https://media.discordapp.net/attachments/938910538799382629/945000454377582654/caption.gif',
           ephemeral: true,
         });
       }
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const Client = require('./client/Client');
 const config = require('./config.json');
 const {Player} = require('discord-player');
-
 const client = new Client();
 client.commands = new Discord.Collection();
 
